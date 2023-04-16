20b2c4882dc6dd1eb4a71e72beab9dbe3ae3f34e
Boris Spektor <20343235+BSpwr@users.noreply.github.com>
2023-04-16 14:09:58 -0400

    clean up

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Dependheal
 
 ## Motivation
-When using docker-compose with the `network_mode: "service:parent_name"` option, or docker with the `--net:container:parent_name` to use the network stack of another container (eg: for tunneling traffic through a VPN), the attached container will permanantly lose its network connection if the parent container restarts.
+When using docker-compose with the `network_mode: "service:parent_name"` option or docker with the `--net:container:parent_name`, the attached container will permanantly lose its network connection if the parent container restarts. 
+
+This mode enables using the network stack of another container (eg: for tunneling traffic through a VPN), but this inability to restart children makes it less useful in scenarios where the parent container sometimes restarts.
 
 There have been issues raised about this for many years on the Docker github, but they were not resolved:
 
@@ -16,13 +18,15 @@ Any container with the `dependheal.parent=parent_name` label will be restarted a
 
 ## Usage
 Run `go mod download` to download dependencies.
+
 Run `go build` to build the `dependheal` executable.
+
 Run the executable with `./dependheal`
 
 ## Usage in Docker
-Run `docker build -t dependheal_img .` to build the docker image.
-Run `docker run -d --name dependheal --restart=unless-stopped -v /var/run/docker.sock:/var/run/docker.sock dependheal_img` to start the docker image.
+Run `docker build -t dependheal .` to build the docker image.
 
+Run `docker run -d --name dependheal --restart=unless-stopped -v /var/run/docker.sock:/var/run/docker.sock dependheal:latest` to start the docker image.
 
 ## Sample usage with docker-compose
 
@@ -31,21 +35,21 @@ version: "3.9"
 services:
   dependheal:
     container_name: dependheal
-    image: dependheal_img
+    image: dependheal:latest
     restart: unless-stopped
     volumes:
       - /var/run/docker.sock:/var/run/docker.sock
 
   parent_container:
     container_name: parent_container
-    image: parent_container_img:latest
+    image: parent:latest
     restart: unless-stopped
     labels:
       - "dependheal.enable=true"
 
   child_container:
     container_name: child_container
-    image: child_container_img:latest
+    image: child:latest
     depends_on:
       - parent_container
     network_mode: service:parent_container
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"strconv"
 	"strings"
 
 	"github.com/docker/docker/api/types"
@@ -53,10 +52,6 @@ func main() {
 			if event.Type == "container" {
 				if event.Action == "start" {
 					if hasLabel(event.Actor.Attributes, "dependheal.enable", "true") {
-						// fmt.Println(event.Type)
-						// fmt.Println(event.Action)
-						// fmt.Printf("%+v\n", event.Actor)
-						// fmt.Println()
 						// quick n dirty, I need a types.Container type
 						var startedContainer types.Container
 						containers, _ := cli.ContainerList(ctx, types.ContainerListOptions{})
@@ -69,8 +64,6 @@ func main() {
 						fmt.Printf("Watching container: %s\n", name)
 						watchedContainers[event.Actor.ID] = startedContainer
 
-						// check if the containers that we are watching depend on this container
-						// if they do, somehow schedule a restart on them
 						for _, container := range watchedContainers {
 							parentName := strings.TrimPrefix(startedContainer.Names[0], "/")
 							if container.ID != startedContainer.ID && hasLabel(container.Labels, "dependheal.parent", parentName) {
@@ -85,8 +78,6 @@ func main() {
 									}
 									fmt.Printf("Restarting container: %s, depends on: %s which just started\n", childName, parentName)
 								}
-
-								// fmt.Printf("CONTAINER: %+v\n", container)
 							}
 						}
 					}
@@ -102,60 +93,6 @@ func main() {
 						// TODO: implement
 					}
 				}
-				// if event.Type == "container" && event.Action == "health_status" || event.Action == "start" {
-				// 	// Check if the container is one of our dependant containers
-				// 	if contains(watchedContainers, event.Actor.ID) {
-				// 		// Find all containers that depend on this container
-				// 		dependentContainers := []string{}
-				// 		for _, depContainer := range containers {
-				// 			if hasLabel(depContainer.Labels, "dependheal.depends_on", event.Actor.Attributes["name"]) {
-				// 				dependentContainers = append(dependentContainers, depContainer.ID)
-				// 			}
-				// 		}
-
-				// 		// Restart dependent containers with delay
-				// 		for _, depContainer := range dependentContainers {
-				// 			depContainerInfo, err := cli.ContainerInspect(ctx, depContainer)
-				// 			if err != nil {
-				// 				panic(err)
-				// 			}
-
-				// 			waitForParent := hasLabel(depContainerInfo.Config.Labels, "dependheal.restart.wait_for_parent", "true")
-				// 			if waitForParent {
-				// 				fmt.Printf("Waiting for container %s to be healthy...\n", event.Actor.Attributes["name"])
-				// 				for {
-				// 					containerInfo, err := cli.ContainerInspect(ctx, event.Actor.ID)
-				// 					if err != nil {
-				// 						panic(err)
-				// 					}
-
-				// 					if containerInfo.State.Status == "running" {
-				// 						break
-				// 					}
-
-				// 					time.Sleep(time.Second)
-				// 				}
-
-				// 				fmt.Printf("Restarting dependent container %s immediately...\n", depContainerInfo.Name)
-				// 				err = cli.ContainerRestart(ctx, depContainer, container.StopOptions{})
-				// 				if err != nil {
-				// 					panic(err)
-				// 				}
-				// 			} else {
-				// 				delay := getLabelInt(depContainerInfo.Config.Labels, "dependheal.restart.delay", 0)
-				// 				if delay > 0 {
-				// 					fmt.Printf("Delaying restart of container %s for %d seconds...\n", depContainerInfo.Name, delay)
-				// 					time.Sleep(time.Duration(delay) * time.Second)
-				// 				}
-
-				// 				fmt.Printf("Restarting dependent container %s...\n", depContainerInfo.Name)
-				// 				err = cli.ContainerRestart(ctx, depContainer, container.StopOptions{})
-				// 				if err != nil {
-				// 					panic(err)
-				// 				}
-				// 			}
-				// 		}
-				// 	}
 			}
 
 		case err := <-eventErrChan:
@@ -170,29 +107,6 @@ func main() {
 func hasLabel(labels map[string]string, key string, value string) bool {
 	if val, ok := labels[key]; ok {
 		return val == value
-	}
-	return false
-}
-
-func getLabelInt(labels map[string]string, key string, defaultValue int) int {
-	valueStr, ok := labels[key]
-	if !ok {
-		return defaultValue
-	}
-
-	value, err := strconv.Atoi(valueStr)
-	if err != nil {
-		panic(fmt.Errorf("Failed to parse label '%s' value '%s' as integer: %w", key, valueStr, err))
-	}
-
-	return value
-}
-
-func contains(slice []string, element string) bool {
-	for _, item := range slice {
-		if item == element {
-			return true
-		}
 	}
 	return false
 }
